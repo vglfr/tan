e5779598f52b42e4ddd5655cbf54fa22c9c5ffb3
Oleksii Dorozhkin <oleksii.dorozhkin@getflowbox.com>
2024-08-22 18:38:22 +0200

    WIP read file into lines

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{Result,stdout};
+use std::fs::File;
+use std::io::{self,BufRead,BufReader,Result,stdout};
 
 use ratatui::{
     backend::CrosstermBackend, crossterm::{
@@ -8,6 +9,7 @@ use ratatui::{
     }, layout::{Position, Rect}, style::Stylize, widgets::Paragraph, Terminal
 };
 
+#[derive(Debug)]
 struct Line {
     n: u16,
     text: String,
@@ -28,111 +30,127 @@ struct Label {
 fn main() -> Result<()> {
     let s = load_content();
 
-    stdout().execute(EnterAlternateScreen)?;
-    enable_raw_mode()?;
+    let l = load_content_lines();
+    dbg!(l);
 
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    terminal.clear()?;
+    // stdout().execute(EnterAlternateScreen)?;
+    // enable_raw_mode()?;
 
-    let mut area = Rect { x: 0, y: 0, height: 0, width: 0 };
-    let mut curpos = Position { x: 0, y: 0 };
+    // let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    // terminal.clear()?;
 
-    let mut v = false;
+    // let mut area = Rect { x: 0, y: 0, height: 0, width: 0 };
+    // let mut curpos = Position { x: 0, y: 0 };
 
-    let mut v_start = Position { x: 0, y: 0 };
-    let mut v_end = Position { x: 0, y: 0 };
+    // let mut v = false;
 
-    loop {
-        terminal.draw(|frame| {
-            area = frame.area();
-            frame.render_widget(
-                Paragraph::new(s.as_str()).white(),
-                area,
-            );
+    // let mut v_start = Position { x: 0, y: 0 };
+    // let mut v_end = Position { x: 0, y: 0 };
 
-            let rect = if v {
-                Rect { x: v_start.x, y: v_start.y, height: v_end.y - v_start.y + 1, width: v_end.x - v_start.x + 1 }
-            } else {
-                Rect { x: 0, y: 0, height: 0, width: 0 }
-            };
+    // loop {
+    //     terminal.draw(|frame| {
+    //         area = frame.area();
+    //         frame.render_widget(
+    //             Paragraph::new(s.as_str()).white(),
+    //             area,
+    //         );
 
-            frame.render_widget(
-                Paragraph::new("").on_green(),
-                rect,
-            );
+    //         let rect = if v {
+    //             Rect { x: v_start.x, y: v_start.y, height: v_end.y - v_start.y + 1, width: v_end.x - v_start.x + 1 }
+    //         } else {
+    //             Rect { x: 0, y: 0, height: 0, width: 0 }
+    //         };
 
-            frame.set_cursor_position(curpos);
-        })?;
+    //         frame.render_widget(
+    //             Paragraph::new("").on_green(),
+    //             rect,
+    //         );
 
-        terminal.show_cursor()?;
+    //         frame.set_cursor_position(curpos);
+    //     })?;
 
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    match key.code {
-                        // :q :w :x
-                        KeyCode::Char('q') => { break; }
-                        // KeyCode::Char('s') => { break; }
-                        // KeyCode::Char('x') => { break; }
+    //     terminal.show_cursor()?;
 
-                        // hjkl movements
-                        KeyCode::Char('h') => {
-                            if curpos.x > 0 {
-                                curpos.x -= 1;
-                            } else if curpos.y > 0 {
-                                curpos.x = area.width - 1;
-                                curpos.y -= 1;
-                            }
-                        }
-                        KeyCode::Char('j') => { curpos.y += if curpos.y < area.height { 1 } else { 0 }; }
-                        KeyCode::Char('k') => { curpos.y -= if curpos.y > 0 { 1 } else { 0 }; }
-                        KeyCode::Char('l') => {
-                            if curpos.x < area.width - 1 {
-                                curpos.x += 1;
-                            } else if curpos.y < area.height {
-                                curpos.x = 0;
-                                curpos.y += 1;
-                            }
-                        }
+    //     if event::poll(std::time::Duration::from_millis(16))? {
+    //         if let event::Event::Key(key) = event::read()? {
+    //             if key.kind == KeyEventKind::Press {
+    //                 match key.code {
+    //                     // :q :w :x
+    //                     KeyCode::Char('q') => { break; }
+    //                     // KeyCode::Char('s') => { break; }
+    //                     // KeyCode::Char('x') => { break; }
 
-                        // wb{} movement (later WB + g-setb)
-                        // KeyCode::Char('w') => { break; }
-                        // KeyCode::Char('b') => { break; }
-                        // KeyCode::Char('{') => { break; }
-                        // KeyCode::Char('}') => { break; }
+    //                     // hjkl movements
+    //                     KeyCode::Char('h') => {
+    //                         if curpos.x > 0 {
+    //                             curpos.x -= 1;
+    //                         } else if curpos.y > 0 {
+    //                             curpos.x = area.width - 1;
+    //                             curpos.y -= 1;
+    //                         }
+    //                     }
+    //                     KeyCode::Char('j') => { curpos.y += if curpos.y < area.height { 1 } else { 0 }; }
+    //                     KeyCode::Char('k') => { curpos.y -= if curpos.y > 0 { 1 } else { 0 }; }
+    //                     KeyCode::Char('l') => {
+    //                         if curpos.x < area.width - 1 {
+    //                             curpos.x += 1;
+    //                         } else if curpos.y < area.height {
+    //                             curpos.x = 0;
+    //                             curpos.y += 1;
+    //                         }
+    //                     }
 
-                        // toggle selection
-                        KeyCode::Char('v') => {
-                            if v {
-                                v = false;
-                                v_end = curpos;
-                            } else {
-                                v = true;
-                                v_start = curpos;
-                                v_end = curpos;
-                            }
-                        }
+    //                     // wb{} movement (later WB + g-setb)
+    //                     // KeyCode::Char('w') => { break; }
+    //                     // KeyCode::Char('b') => { break; }
+    //                     // KeyCode::Char('{') => { break; }
+    //                     // KeyCode::Char('}') => { break; }
 
-                        // tag-untag
-                        // KeyCode::Char('t') => { break; }
-                        // KeyCode::Char('u') => { break; }
+    //                     // toggle selection
+    //                     KeyCode::Char('v') => {
+    //                         if v {
+    //                             v = false;
+    //                             v_end = curpos;
+    //                         } else {
+    //                             v = true;
+    //                             v_start = curpos;
+    //                             v_end = curpos;
+    //                         }
+    //                     }
 
-                        _ => {}
-                    }
-                }
-            }
-        }
-    }
+    //                     // tag-untag
+    //                     // KeyCode::Char('t') => { break; }
+    //                     // KeyCode::Char('u') => { break; }
 
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
+    //                     _ => {}
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // stdout().execute(LeaveAlternateScreen)?;
+    // disable_raw_mode()?;
 
     Ok(())
 }
 
 fn load_content() -> String {
-    // read to file
-    // prevent from opening non-utf8
     let f = std::env::args().nth(1).unwrap_or("flake.nix".to_string());
     std::fs::read_to_string(f).unwrap()
+}
+
+fn load_content_lines() -> io::Result<Line> {
+    let fname = std::env::args().nth(1).unwrap_or("flake.nix".to_string());
+    let f = File::open(fname).unwrap();
+    let mut b = BufReader::new(f);
+
+    let mut s = String::new();
+    b.read_line(&mut s);
+    // s.strip_suffix();
+
+    let l = Line { n: 0, text: s, width: 0 };
+    Ok(l)
+
+    // prevent from opening non-utf8
 }
