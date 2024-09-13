d21b5aa539061e074a6f858799d98780811ee4d1
vglfr <vf.velt@gmail.com>
2024-09-12 22:23:22 -0400

    Implement visual selection in a line

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 struct Label {
     name: String,
-    color: String,
+    color: Color,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -33,32 +33,41 @@ struct Tag {
 }
 
 fn main() -> std::io::Result<()> {
+    let fname = std::env::args().nth(1).unwrap_or("flake.nix".to_string());
+    let qname = format!("data/{fname}.tan");
+
+    let mut v = false;
+    let mut v_start = 0;
+
     let mut stdout = std::io::stdout();
     terminal::enable_raw_mode()?;
 
-    let lines = load_file()?;
+    let lines = load_file(&fname, &qname)?;
     execute!(stdout, terminal::EnterAlternateScreen)?;
 
-    print_lines(&lines, &mut stdout)?;
     execute!(stdout, cursor::MoveTo(0, 0))?;
-
-    // let mut v = false;
-
-    // let mut v_start = Position { x: 0, y: 0 };
-    // let mut v_end = Position { x: 0, y: 0 };
+    render_screen(&lines, &mut stdout, v, v_start)?;
 
     loop {
         match read()? {
             Event::Key(event) => match event.code {
                 KeyCode::Char(c) => match c {
                     'q' => break,
-                    'w' => save_tan(&lines)?,
+                    'w' => save_tan(&lines, &qname)?,
                     'h' => {
-                        execute!(stdout, cursor::MoveLeft(1))?
+                        execute!(stdout, cursor::MoveLeft(1))?;
+                        if v { render_screen(&lines, &mut stdout, v, v_start)?; }
                     },
-                    'j' => execute!(stdout, cursor::MoveDown(1))?,
-                    'k' => execute!(stdout, cursor::MoveUp(1))?,
-                    'l' => execute!(stdout, cursor::MoveRight(1))?,
+                    'j' => {
+                        execute!(stdout, cursor::MoveDown(1))?;
+                    },
+                    'k' => {
+                        execute!(stdout, cursor::MoveUp(1))?;
+                    },
+                    'l' => {
+                        execute!(stdout, cursor::MoveRight(1))?;
+                        if v { render_screen(&lines, &mut stdout, v, v_start)?; }
+                    },
 //                     // :q :w :x
 //                     KeyCode::Char('q') => { break; }
 //                     // KeyCode::Char('w') => { break; }
@@ -90,17 +99,19 @@ fn main() -> std::io::Result<()> {
 //                     // KeyCode::Char('{') => { break; }
 //                     // KeyCode::Char('}') => { break; }
 
-//                     // toggle selection
-//                     KeyCode::Char('v') => {
-//                         if v {
-//                             v = false;
-//                             v_end = curpos;
-//                         } else {
-//                             v = true;
-//                             v_start = curpos;
-//                             v_end = curpos;
-//                         }
-//                     }
+                    // toggle selection
+                    'v' => {
+                        if v {
+                            v = false;
+                            execute!(stdout, cursor::SetCursorStyle::SteadyBlock)?;
+                        } else {
+                            v = true;
+                            v_start = cursor::position().unwrap().0;
+
+                            execute!(stdout, cursor::SetCursorStyle::SteadyUnderScore)?;
+                            render_screen(&lines, &mut stdout, v, v_start)?;
+                        }
+                    }
 
 //                     // tag-untag
 //                     // KeyCode::Char('t') => { break; }
@@ -125,37 +136,58 @@ fn tag() {
 fn untag() {
 }
 
-fn print_lines(lines: &Vec<Line>, stdout: &mut Stdout) -> std::io::Result<()> {
+fn render_screen(lines: &Vec<Line>, stdout: &mut Stdout, v: bool, v_start: u16) -> std::io::Result<()> {
+    let (v_end, row) = cursor::position().unwrap();
+    execute!(stdout, cursor::SavePosition)?;
+
     for line in lines {
-        for tag in &line.tags {
+        let mut tmp: Vec<Tag> = vec![];
+        let chunks = if v && line.row == row {
+            let mut a = [v_start as usize, v_end as usize];
+            a.sort();
+
+            tmp.push(Tag { start: 0, end: a[0], label: Label { name: "0".to_owned(), color: Color::Reset }});
+            tmp.push(Tag { start: a[0], end: a[1] + 1, label: Label { name: "0".to_owned(), color: Color::Yellow }});
+            tmp.push(Tag { start: a[1] + 1, end: line.width as usize, label: Label { name: "0".to_owned(), color: Color::Reset }});
+
+            // let s = serde_json::to_string(&tmp)?;
+            // let mut f = File::create("/tmp/dbg.json")?;
+            // f.write_all(s.as_bytes())?;
+            // f.write_all(serde_json::to_string(&line)?.as_bytes())?;
+
+            &tmp
+        } else { &line.tags };
+
+        for chunk in chunks {
             queue!(
                 stdout,
-                cursor::MoveTo(tag.start as u16, line.row),
-                style::SetBackgroundColor(if tag.label.name == "0" { Color::Reset } else { Color::Red }),
-                style::Print(&line.text[tag.start..tag.end])
+                cursor::MoveTo(chunk.start as u16, line.row),
+                style::SetBackgroundColor(chunk.label.color),
+                style::Print(&line.text[chunk.start..chunk.end])
             )?;
         }
     }
 
+    execute!(stdout, cursor::RestorePosition)?;
     stdout.flush()?;
+
     Ok(())
 }
 
-fn load_file() -> std::io::Result<Vec<Line>> {
-    let fname = std::env::args().nth(1).unwrap_or("flake.nix".to_string());
-    if std::path::Path::new("flake.nix.tan").exists() {
-        load_tan(fname)
+fn load_file(fname: &str, qname: &str) -> std::io::Result<Vec<Line>> {
+    if std::path::Path::new(&qname).exists() {
+        load_tan(qname)
     } else {
         load_src(fname)
     }
 }
 
-fn load_tan(fname: String) -> std::io::Result<Vec<Line>> {
+fn load_tan(fname: &str) -> std::io::Result<Vec<Line>> {
     let s = std::fs::read_to_string(fname)?;
     serde_json::from_str(&s).map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
 }
 
-fn load_src(fname: String) -> std::io::Result<Vec<Line>> {
+fn load_src(fname: &str) -> std::io::Result<Vec<Line>> {
     let f = File::open(fname).unwrap();
     let mut b = BufReader::new(f);
 
@@ -168,15 +200,15 @@ fn load_src(fname: String) -> std::io::Result<Vec<Line>> {
             row: n,
             text: s.strip_suffix("\n").map(|x| x.to_owned()).unwrap_or(s.clone()),
             width: s.len() as u16 - 1,
-            tags: vec![Tag { start: 0, end: s.len() - 1, label: Label { name: "0".to_owned(), color: "0".to_owned() } }],
+            tags: vec![Tag { start: 0, end: s.len() - 1, label: Label { name: "0".to_owned(), color: Color::Reset } }],
         };
         n += 1;
 
         if n == 2 {
             l.tags = vec![
-                Tag { start: 0, end: 2, label: Label { name: "0".to_owned(), color: "0".to_owned() } },
-                Tag { start: 2, end: 8, label: Label { name: "foo".to_owned(), color: "bar".to_owned() } },
-                Tag { start: 8, end: 12, label: Label { name: "0".to_owned(), color: "0".to_owned() } },
+                Tag { start: 0, end: 2, label: Label { name: "0".to_owned(), color: Color::Reset } },
+                Tag { start: 2, end: 8, label: Label { name: "foo".to_owned(), color: Color::Red } },
+                Tag { start: 8, end: 12, label: Label { name: "0".to_owned(), color: Color::Reset } },
             ];
         }
 
@@ -189,9 +221,9 @@ fn load_src(fname: String) -> std::io::Result<Vec<Line>> {
     // prevent from opening non-utf8
 }
 
-fn save_tan(lines: &Vec<Line>) -> std::io::Result<()> {
+fn save_tan(lines: &Vec<Line>, qname: &str) -> std::io::Result<()> {
     let s = serde_json::to_string(lines)?;
-    let mut f = File::create("flake.nix.tan")?;
+    let mut f = File::create(qname)?;
 
     f.write_all(s.as_bytes())?;
     Ok(())
