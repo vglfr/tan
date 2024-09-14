1fe8156d050ebf744a81dbb6bdff45ec3db7dd7f
vglfr <vf.velt@gmail.com>
2024-09-13 21:43:48 -0400

    WIP tag / untag implementation

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,28 @@ use crossterm::{
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+struct App {
+    fname: String,
+    qname: String,
+    cursor_column: u16,
+    cursor_row: u16,
+    height: u16,
+    labels: Vec<Label>,
+    lines: Vec<Line>,
+    visual: bool,
+    visual_row: u16,
+    visual_start: u16,
+    visual_end: u16,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Chunk {
+    start: u16,
+    end: u16,
+    color: Color,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct Label {
     name: String,
     color: Color,
@@ -25,10 +47,10 @@ struct Line {
     tags: Vec<Tag>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct Tag {
-    start: usize,
-    end: usize,
+    start: u16,
+    end: u16,
     label: Label,
 }
 
@@ -36,93 +58,93 @@ fn main() -> std::io::Result<()> {
     let fname = std::env::args().nth(1).unwrap_or("flake.nix".to_string());
     let qname = format!("data/{fname}.tan");
 
-    let mut v = false;
-    let mut v_start = 0;
-
     let mut stdout = std::io::stdout();
     terminal::enable_raw_mode()?;
 
-    let lines = load_file(&fname, &qname)?;
+    let mut app = load_file(&fname, &qname)?;
     execute!(stdout, terminal::EnterAlternateScreen)?;
 
-    execute!(stdout, cursor::MoveTo(0, 0))?;
-    render_screen(&lines, &mut stdout, v, v_start)?;
+    execute!(stdout, cursor::MoveTo(app.cursor_column, app.cursor_row))?;
+    render_screen(&app, &mut stdout)?;
 
     loop {
         match read()? {
             Event::Key(event) => match event.code {
                 KeyCode::Char(c) => match c {
-                    // :q :w :x
                     'q' => break,
-                    'w' => save_tan(&lines, &qname)?,
+                    'w' => save_tan(&app)?,
                     // KeyCode::Char('x') => { break; }
 
-                    // hjkl movements
                     'h' => {
-                        let (c, r) = cursor::position()?;
-                        if c > 0 {
-                            execute!(stdout, cursor::MoveLeft(1))?;
-                        } else if r > 0 {
-                            execute!(stdout, cursor::MoveTo(lines[r.saturating_sub(1) as usize].width.saturating_sub(1), r.saturating_sub(1)))?;
+                        if app.cursor_column > 0 {
+                            app.cursor_column = app.cursor_column.saturating_sub(1);
+                        } else if app.cursor_row > 0 {
+                            app.cursor_column = app.lines[app.cursor_row.saturating_sub(1) as usize].width.saturating_sub(1);
+                            app.cursor_row = app.cursor_row.saturating_sub(1);
                         }
-                        if v { render_screen(&lines, &mut stdout, v, v_start)?; }
                     },
                     'j' => {
-                        let (c, r) = cursor::position()?;
-                        if r < lines.len().saturating_sub(1) as u16 {
-                            execute!(stdout, cursor::MoveDown(1))?;
-                            if c >= lines[r as usize + 1].width {
-                                execute!(stdout, cursor::MoveToColumn(lines[r as usize + 1].width.saturating_sub(1)))?;
+                        if app.cursor_row < app.height.saturating_sub(1) {
+                            app.cursor_row += 1;
+
+                            if app.cursor_column >= app.lines[app.cursor_row as usize].width {
+                                app.cursor_column = app.lines[app.cursor_row as usize].width.saturating_sub(1);
                             }
                         }
                     },
                     'k' => {
-                        let (c, r) = cursor::position()?;
-                        if r < lines.len() as u16 {
-                            execute!(stdout, cursor::MoveUp(1))?;
-                            if c >= lines[r.saturating_sub(1) as usize].width {
-                                execute!(stdout, cursor::MoveToColumn(lines[r.saturating_sub(1) as usize].width.saturating_sub(1)))?;
+                        if app.cursor_row < app.height {
+                            app.cursor_row = app.cursor_row.saturating_sub(1);
+
+                            if app.cursor_column >= app.lines[app.cursor_row as usize].width {
+                                app.cursor_column = app.lines[app.cursor_row as usize].width.saturating_sub(1);
                             }
                         }
                     },
                     'l' => {
-                        let (c, r) = cursor::position()?;
-                        if c < lines[r as usize].width.saturating_sub(1) {
-                            execute!(stdout, cursor::MoveRight(1))?;
-                        } else if r < lines.len().saturating_sub(1) as u16 {
-                            execute!(stdout, cursor::MoveTo(0, r + 1))?;
+                        if app.cursor_column < app.lines[app.cursor_row as usize].width.saturating_sub(1) {
+                            app.cursor_column += 1;
+                        } else if app.cursor_row < app.height.saturating_sub(1) {
+                            app.cursor_column = 0;
+                            app.cursor_row += 1;
                         }
-                        if v { render_screen(&lines, &mut stdout, v, v_start)?; }
                     },
 
-//                     // wb{} movement (later WB + g-setb)
-//                     // KeyCode::Char('w') => { break; }
-//                     // KeyCode::Char('b') => { break; }
-//                     // KeyCode::Char('{') => { break; }
-//                     // KeyCode::Char('}') => { break; }
+                    // wb{} movement (later WB + g-seSE)
+                    // KeyCode::Char('w') => { break; }
+                    // KeyCode::Char('b') => { break; }
+                    // KeyCode::Char('{') => { break; }
+                    // KeyCode::Char('}') => { break; }
 
                     // toggle selection
                     'v' => {
-                        if v {
-                            v = false;
-                            execute!(stdout, cursor::SetCursorStyle::SteadyBlock)?;
+                        if app.visual {
+                            app.visual = false;
+                            app.visual_end = app.cursor_column;
                         } else {
-                            v = true;
-                            v_start = cursor::position().unwrap().0;
-
-                            execute!(stdout, cursor::SetCursorStyle::SteadyUnderScore)?;
-                            render_screen(&lines, &mut stdout, v, v_start)?;
+                            app.visual = true;
+                            app.visual_start = app.cursor_column;
+                            app.visual_row = app.cursor_row;
                         }
                     }
 
-//                     // tag-untag
-//                     // KeyCode::Char('t') => { break; }
-//                     // KeyCode::Char('u') => { break; }
+                    't' => {
+                        tag(&mut app);
+                        app.visual = false;
+                    },
+                    'u' => { untag(&mut app); },
+
                     _ => (),
                     },
                 _ => (),
             },
             _ => (),
+        }
+
+        execute!(stdout, cursor::MoveTo(app.cursor_column, app.cursor_row))?;
+        if app.visual {
+            app.visual_end = app.cursor_column;
+            render_screen(&app, &mut stdout)?;
         }
     }
 
@@ -132,43 +154,31 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn tag() {
+fn tag(app: &mut App) {
+    let mut a = [app.visual_start, app.visual_end];
+    a.sort();
+
+    let s = a[0];
+    let e = std::cmp::min(a[1] + 1, app.lines[app.visual_row as usize].width);
+
+    app.lines[app.visual_row as usize].tags.push(Tag { start: s, end: e, label: Label { name: "tag1".to_owned(), color: Color::Red }});
 }
 
-fn untag() {
+fn untag(app: &mut App) {
+    let tags = app.lines[app.cursor_row as usize].tags.clone();
+    app.lines[app.cursor_row as usize].tags = tags.into_iter().filter(|x| !(x.start <= app.cursor_column && app.cursor_column <= x.end)).collect::<_>();
 }
 
-fn render_screen(lines: &Vec<Line>, stdout: &mut Stdout, v: bool, v_start: u16) -> std::io::Result<()> {
-    let (v_end, row) = cursor::position().unwrap();
+fn render_screen(app: &App, stdout: &mut Stdout) -> std::io::Result<()> {
     execute!(stdout, cursor::SavePosition)?;
 
-    for line in lines {
-        let mut tmp: Vec<Tag> = vec![];
-        let chunks = if v && line.row == row {
-            let mut a = [v_start as usize, v_end as usize];
-            a.sort();
-
-            let s = a[0];
-            let e = std::cmp::min(a[1] + 1, line.width as usize);
-
-            tmp.push(Tag { start: 0, end: s, label: Label { name: "0".to_owned(), color: Color::Reset }});
-            tmp.push(Tag { start: s, end: e, label: Label { name: "0".to_owned(), color: Color::Yellow }});
-            tmp.push(Tag { start: e, end: line.width as usize, label: Label { name: "0".to_owned(), color: Color::Reset }});
-
-            // let s = serde_json::to_string(&tmp)?;
-            // let mut f = File::create("/tmp/dbg.json")?;
-            // f.write_all(s.as_bytes())?;
-            // f.write_all(serde_json::to_string(&line)?.as_bytes())?;
-
-            &tmp
-        } else { &line.tags };
-
-        for chunk in chunks {
+    for line in &app.lines {
+        for chunk in chunk_line(line, app) {
             queue!(
                 stdout,
-                cursor::MoveTo(chunk.start as u16, line.row),
-                style::SetBackgroundColor(chunk.label.color),
-                style::Print(&line.text[chunk.start..chunk.end])
+                cursor::MoveTo(chunk.start, line.row),
+                style::SetBackgroundColor(chunk.color),
+                style::Print(&line.text[chunk.start.into()..chunk.end.into()]),
             )?;
         }
     }
@@ -179,56 +189,88 @@ fn render_screen(lines: &Vec<Line>, stdout: &mut Stdout, v: bool, v_start: u16) 
     Ok(())
 }
 
-fn load_file(fname: &str, qname: &str) -> std::io::Result<Vec<Line>> {
+fn chunk_line(line: &Line, app: &App) -> Vec<Chunk> {
+    let mut tags = Vec::new();
+
+    if app.visual && line.row == app.visual_row {
+        let mut a = [app.visual_start, app.visual_end];
+        a.sort();
+
+        let s = a[0];
+        let e = std::cmp::min(a[1] + 1, line.width);
+
+        tags.push(Chunk { start: 0, end: s, color: Color::Reset });
+        tags.push(Chunk { start: s, end: e, color: Color::Yellow });
+        tags.push(Chunk { start: e, end: line.width, color: Color::Reset });
+
+        // let s = serde_json::to_string(&tags).unwrap();
+        // let mut f = File::create("/tmp/dbg.json").unwrap();
+        // f.write_all(s.as_bytes()).unwrap();
+        // f.write_all(serde_json::to_string(&line).unwrap().as_bytes()).unwrap();
+
+    } else {
+        tags.push(Chunk { start: 0, end: line.width, color: Color::Reset });
+    }
+
+    tags
+}
+
+fn load_file(fname: &str, qname: &str) -> std::io::Result<App> {
     if std::path::Path::new(&qname).exists() {
         load_tan(qname)
     } else {
-        load_src(fname)
+        load_src(fname, qname)
     }
 }
 
-fn load_tan(fname: &str) -> std::io::Result<Vec<Line>> {
-    let s = std::fs::read_to_string(fname)?;
+fn load_tan(qname: &str) -> std::io::Result<App> {
+    let s = std::fs::read_to_string(qname)?;
     serde_json::from_str(&s).map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
 }
 
-fn load_src(fname: &str) -> std::io::Result<Vec<Line>> {
+fn load_src(fname: &str, qname: &str) -> std::io::Result<App> {
     let f = File::open(fname).unwrap();
     let mut b = BufReader::new(f);
 
-    let mut ls = Vec::new();
+    let mut lines = Vec::new();
     let mut s = String::new();
     let mut n = 0;
 
     while b.read_line(&mut s).unwrap_or(0) > 0 {
-        let mut l = Line {
+        let l = Line {
             row: n,
             text: s.strip_suffix("\n").map(|x| x.to_owned()).unwrap_or(s.clone()),
             width: s.len() as u16 - 1,
-            tags: vec![Tag { start: 0, end: s.len() - 1, label: Label { name: "0".to_owned(), color: Color::Reset } }],
+            tags: Vec::new(),
         };
+        lines.push(l);
+
         n += 1;
-
-        if n == 2 {
-            l.tags = vec![
-                Tag { start: 0, end: 2, label: Label { name: "0".to_owned(), color: Color::Reset } },
-                Tag { start: 2, end: 8, label: Label { name: "foo".to_owned(), color: Color::Red } },
-                Tag { start: 8, end: 12, label: Label { name: "0".to_owned(), color: Color::Reset } },
-            ];
-        }
-
-        ls.push(l);
         s.truncate(0);
     }
 
-    Ok(ls)
+    let app = App {
+        fname: fname.to_owned(),
+        qname: qname.to_owned(),
+        cursor_column: 0,
+        cursor_row: 0,
+        height: lines.len() as u16,
+        labels: Vec::new(),
+        lines,
+        visual: false,
+        visual_row: 0,
+        visual_start: 0,
+        visual_end: 0,
+    };
+
+    Ok(app)
 
     // prevent from opening non-utf8
 }
 
-fn save_tan(lines: &Vec<Line>, qname: &str) -> std::io::Result<()> {
-    let s = serde_json::to_string(lines)?;
-    let mut f = File::create(qname)?;
+fn save_tan(app: &App) -> std::io::Result<()> {
+    let s = serde_json::to_string(app)?;
+    let mut f = File::create(&app.qname)?;
 
     f.write_all(s.as_bytes())?;
     Ok(())
